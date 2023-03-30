1d1ce3eec895acd52ac48c43f270913f3bd49184
lukaszliwinski <lukaszsliwinski0@gmail.com>
2023-03-30 08:08:01 +0200

    initial commit - starter configuration

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+// required dependencies
+const path = require('path');
+const express = require('express');
+const app = express();
+
+// dotenv package
+require('dotenv').config();
+
+// port number
+const PORT = process.env.PORT || 4001;
+
+// middleware
+app.use(express.static('public'));
+app.use(express.static(path.resolve(__dirname, '../frontend/build')));
+
+// render react app
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
+});
+
+// run server
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}\nenv: ${process.env.NODE_ENV}`);
+});
