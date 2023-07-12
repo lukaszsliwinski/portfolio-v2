1731009e538e00e1119a4ef29b30379695ce0e65
Łukasz Śliwiński <74149405+lukaszsliwinski@users.noreply.github.com>
2023-07-12 12:13:09 +0200

    refactor: rename main component

--- frontend/src/Portfolio.tsx
+++ frontend/src/Portfolio.tsx
@@ -9,7 +9,7 @@ import WebApps from './layouts/WebApps';
 import Websites from './layouts/Websites';
 import MobileApps from './layouts/MobileApps';
 
-export default function Website() {
+export default function Portfolio() {
   return (
     <div className="min-h-screen-mobile overflow-x-hidden bg-white tracking-wider dark:bg-neutral-900 dark:text-neutral-200">
       <Nav />
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './assets/global.css';
-import Website from './Website';
+import Portfolio from './Portfolio';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <Website />
+    <Portfolio />
   </React.StrictMode>
 );
 
