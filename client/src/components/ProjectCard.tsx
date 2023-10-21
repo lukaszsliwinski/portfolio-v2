a9792da6ebc50cc700c7c3e300582765c8ae7745
lukaszliwinski <lukaszsliwinski0@gmail.com>
2023-10-21 20:52:45 +0200

    feat: rebuild page layout (src/components)

--- client/src/components/ProjectCard.tsx
+++ client/src/components/ProjectCard.tsx
@@ -16,14 +16,14 @@ export default function PorjectCard({
   linkUrl
 }: IProjectCard) {
   return (
-    <div className="mx-auto max-w-sm rounded-lg border border-neutral-200 bg-white shadow dark:border-neutral-700 dark:bg-neutral-800">
-      <img className="rounded-t-lg" src={screenSrc} alt="apps screen" />
+    <div className="mx-auto max-w-sm rounded-lg border-8 border-main-mid bg-main-mid text-neutral-200 shadow-lg">
+      <img src={screenSrc} alt="apps screen" />
       <div className="flex flex-col p-5 lg:h-72">
-        <h3 className="xs:text-xl text-lg font-semibold tracking-tight text-neutral-900 dark:text-neutral-200">
+        <h3 className="xs:text-xl text-lg font-semibold tracking-tight">
           {title}
         </h3>
         <div className="flex items-center space-x-2 py-3">{children}</div>
-        <p className="xs:text-sm mb-3 text-justify text-xs leading-6 text-neutral-700 dark:text-neutral-400">
+        <p className="xs:text-sm mb-3 text-justify text-xs leading-6">
           {description}
         </p>
         <div className="mt-auto flex flex-wrap justify-evenly">
@@ -33,7 +33,7 @@ export default function PorjectCard({
             rel="noopener noreferrer"
             className={`${
               showSource ? 'inline-flex' : 'hidden'
-            } m-1 items-center rounded-lg bg-indigo-700 px-3 py-2 text-center text-sm font-medium text-neutral-100 hover:bg-indigo-800 focus:outline-none focus:ring-4 focus:ring-indigo-300 dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-800`}
+            } m-1 inline-flex items-center rounded-lg border border-neutral-200 px-3 py-2 text-center text-sm font-medium text-neutral-200 hover:text-main-dark hover:bg-neutral-200 focus:outline-none focus:ring-4 focus:ring-neutral-200`}
           >
             Code
             <Code className="ml-2 h-4 w-4" aria-hidden="true" />
@@ -44,7 +44,7 @@ export default function PorjectCard({
             rel="noopener noreferrer"
             className={`${
               showLink ? 'inline-flex' : 'hidden'
-            } m-1 items-center rounded-lg bg-indigo-700 px-3 py-2 text-center text-sm font-medium text-neutral-100 hover:bg-indigo-800 focus:outline-none focus:ring-4 focus:ring-indigo-300 dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-800`}
+            } m-1 inline-flex items-center rounded-lg border border-neutral-200 px-3 py-2 text-center text-sm font-medium text-neutral-200 hover:text-main-dark hover:bg-neutral-200 focus:outline-none focus:ring-4 focus:ring-neutral-200`}
           >
             Link
             <Link className="ml-2 h-4 w-4" aria-hidden="true" />
