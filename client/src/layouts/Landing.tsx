b58b0fc8864a8f4d5a465ccf436d62ddf09f7c14
lukaszliwinski <lukaszsliwinski0@gmail.com>
2023-11-05 16:29:38 +0100

    feat: update landing page

--- client/src/layouts/Landing.tsx
+++ client/src/layouts/Landing.tsx
@@ -9,24 +9,28 @@ export default function Landing() {
   const { t } = useTranslation();
 
   return (
-    <section
-     className="flex flex-col justify-center items-center space-y-6 h-full min-h-screen w-screen bg-wallpaper text-neutral-200"
-     id="home"
+    <div
+      className="h-screen-mobile top-0 bg-cover bg-fixed bg-center bg-wallpaper"
+      id="home"
     >
-      <img
-        className="w-40 rounded-full"
-        src={profilePhoto}
-        alt="profile photo"
-      />
-      <hgroup className="flex flex-col items-center space-y-2 tracking-widest">
-        <h1 className="text-5xl font-medium">ŁUKASZ ŚLIWIŃSKI</h1>
-        <h2 className="text-3xl">FRONT-END DEVELOPER</h2>
-      </hgroup>
+      <section
+      className="flex flex-col justify-center items-center space-y-6 h-full min-h-screen w-screen bg-main-dark/30 text-neutral-200"
+      >
+        <img
+          className="w-44 rounded-full"
+          src={profilePhoto}
+          alt="profile photo"
+        />
+        <hgroup className="flex flex-col items-center space-y-2 tracking-widest">
+          <h1 className="text-6xl font-medium">ŁUKASZ ŚLIWIŃSKI</h1>
+          <h2 className="text-3xl">FRONT-END DEVELOPER</h2>
+        </hgroup>
 
-      <a href="#about" className="m-1 inline-flex items-center rounded-lg border border-neutral-200 px-3 py-2 text-center text-sm font-medium text-neutral-200 hover:text-main-dark hover:bg-neutral-200 focus:outline-none focus:ring-4 focus:ring-neutral-200">
-        <span>{t('about')}</span>
-      </a>
-      <div className="h-16"></div>
-    </section>
+        <a href="#about" className="m-1 inline-flex items-center rounded-lg border border-neutral-200 px-3 py-2 text-center text-sm font-medium text-neutral-200 hover:text-main-dark hover:bg-neutral-200 focus:outline-none focus:ring-4 focus:ring-neutral-200">
+          <span>{t('about')}</span>
+        </a>
+        {/* <div className="h-16"></div> */}
+      </section>
+    </div>
   );
 }
