aecc584f6aea00d218b5779430bfc9d4ae44239c
lukaszliwinski <lukaszsliwinski0@gmail.com>
2024-01-07 13:13:58 +0100

    feat: implement moment.js on page footer

--- client/src/layouts/Footer.tsx
+++ client/src/layouts/Footer.tsx
@@ -1,5 +1,9 @@
+import moment from 'moment';
+
 export default function Footer() {
+  const year = moment().year();
+
   return (
-    <footer className="w-100 mb-2 mt-8 text-center text-xs">&copy; 2023-2024 ŁUKASZ ŚLIWIŃSKI</footer>
+    <footer className="w-100 mb-2 mt-8 text-center text-xs">&copy; 2023-{year} ŁUKASZ ŚLIWIŃSKI</footer>
   );
 }
