fd94765482e0519133a358f3e3e75035b4f0278f
lukaszsliwinski <lukaszsliwinski0@gmail.com>
2025-02-13 14:52:05 +0100

    feat: settle project cards inside carousel

--- client/src/Portfolio.tsx
+++ client/src/Portfolio.tsx
@@ -7,7 +7,6 @@ import Nav from './layouts/Nav';
 import Landing from './layouts/Landing';
 import About from './layouts/About';
 import Technologies from './layouts/Technologies';
-import ProjectsOld from './layouts/ProjectsOld';
 import Projects from './layouts/Projects';
 import Footer from './layouts/Footer';
 
--- client/src/layouts/Projects.tsx
+++ client/src/layouts/Projects.tsx
@@ -1,21 +1,102 @@
 "use client";
 
+// packages
+import { useTranslation } from 'react-i18next';
 import { Carousel } from "flowbite-react";
 
+// technologies icons
+import typeScript from '../assets/icons/typescript.png';
+import javaScript from '../assets/icons/javascript.png';
+import angular from '../assets/icons/angular.png';
+import react from '../assets/icons/react.png';
+import scss from '../assets/icons/scss.png';
+import tailwind from '../assets/icons/tailwind.png';
+import node from '../assets/icons/node.png';
+import mongo from '../assets/icons/mongo.png';
+
+// project screens
+import radioScreen from '../assets/img/radio-screen.png';
+import vocabularyQuizScreen from '../assets/img/vocabulary-quiz-screen.png';
+import ksmbScreen from '../assets/img/ksmb-screen.png';
+
+// components
+import { XyzTransition } from '@animxyz/react';
+import ProjectCard from '../components/ProjectCard';
+import TechIcon from '../components/TechIcon';
+
+
 export default function Projects() {
+  // useTranslation fn
+  const { t } = useTranslation();
+
   return (
-    <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
+    <section
+      className="xs:px-6 h-full min-h-screen flex-col items-center justify-center px-2"
+      id="projects"
+    >
+      <XyzTransition
+        xyz="fade flip-left perspective-5 duration-10"
+        appearVisible={{ threshold: 0.5, rootMargin: '0px' }}
+      >
+        <header className="my-20 flex w-full flex-col items-center">
+          <h2 className="2xs:text-2xl text-xl font-medium sm:text-3xl">{t('projects')}</h2>
+          <div className="bg-main-light mt-2 h-0.5 w-6 rounded"></div>
+        </header>
+      </XyzTransition>
+
       <Carousel>
-        <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
-          Slide 1
-        </div>
-        <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
-          Slide 2
-        </div>
-        <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
-          Slide 3
-        </div>
+        {/* Radio project card */}
+        <ProjectCard
+          screenSrc={radioScreen}
+          title={'Radio App'}
+          description={t('radio')}
+          showSource={true}
+          sourceUrl={'https://github.com/lukaszsliwinski/radio/'}
+          showLink={true}
+          linkUrl={'https://radio.lukaszsliwinski.pl'}
+        >
+          <TechIcon project="Radio" tech="TypeScript" icon={typeScript} />
+          <TechIcon project="Radio" tech="Angular" icon={angular} />
+          <TechIcon project="Radio" tech="Scss" icon={scss} />
+          <TechIcon project="Radio" tech="Node.js" icon={node} />
+          <TechIcon project="Radio" tech="MongoDB" icon={mongo} />
+        </ProjectCard>
+
+        {/* Vocabulary Quiz project card */}
+        <ProjectCard
+          screenSrc={vocabularyQuizScreen}
+          title={'Vocabulary Quiz'}
+          description={t('vocabularyquiz')}
+          showSource={true}
+          sourceUrl={'https://github.com/lukaszsliwinski/vocabulary-quiz/'}
+          showLink={true}
+          linkUrl={'https://vocabularyquiz.lukaszsliwinski.pl'}
+        >
+          <TechIcon project="VocabularyQuiz" tech="TypeScript" icon={typeScript} />
+          <TechIcon project="VocabularyQuiz" tech="Angular" icon={angular} />
+          <TechIcon project="VocabularyQuiz" tech="Scss" icon={scss} />
+          <TechIcon project="VocabularyQuiz" tech="Node.js" icon={node} />
+          <TechIcon project="VocabularyQuiz" tech="MongoDB" icon={mongo} />
+        </ProjectCard>
+        
+        {/* KSMicroband project card */}
+        <ProjectCard
+          screenSrc={ksmbScreen}
+          title={'KSMicroband Website'}
+          description={t('ksmicroband')}
+          showSource={false}
+          sourceUrl={'#'}
+          showLink={true}
+          linkUrl={'https://ksmicroband.pl/'}
+        >
+          <TechIcon project="KSMicroband" tech="JavaScript" icon={javaScript} />
+          <TechIcon project="KSMicroband" tech="React.js" icon={react} />
+          <TechIcon project="KSMicroband" tech="Tailwindcss" icon={tailwind} />
+          <TechIcon project="KSMicroband" tech="Node.js" icon={node} />
+        </ProjectCard>
       </Carousel>
-    </div>
+    </section>
+
+  
   );
 }
